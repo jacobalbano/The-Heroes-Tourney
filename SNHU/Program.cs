944f47d1b581099f6f187b513efd1a05747292f1
Jacob Albano <Jacob Albano@localhost>
2013-11-15 18:59:30 -0500

    Initial commit;

---
+++ SNHU/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Jake
+ * Date: 11/15/2013
+ * Time: 6:12 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using Punk;
+
+namespace SNHU
+{
+	class Program : Engine
+	{
+		public Program() : base(1000, 640, 60) {}
+		
+		public static void Main(string[] args)
+		{
+			var game = new Program();
+		}
+	}
+}
