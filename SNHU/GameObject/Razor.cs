028f7c959478af6d890d8c6f8634550eaf3b6e14
Jacob Albano <Jacob Albano@localhost>
2013-11-16 11:41:17 -0500

    Razor blade is now a separate entity with potential for dealing damage; Added more graphics for razor blade; Scaled player graphics down slightly;

--- SNHU/GameObject/Razor.cs
+++ SNHU/GameObject/Razor.cs
@@ -12,6 +12,28 @@ using Punk.Graphics;
 
 namespace SNHU.GameObject
 {
+	public class RazorBlade : Entity
+	{
+		public Image blade;
+		
+		public RazorBlade()
+		{
+			blade = new Image(Library.GetTexture("assets/razor.png"));
+			blade.Scale = FP.Random + 1f;
+			blade.CenterOO();
+			SetHitboxTo(blade);
+			CenterOrigin();
+			
+			Graphic = blade;
+		}
+		
+		public override void Update()
+		{
+			base.Update();
+			blade.Angle += 15;
+		}
+	}
+	
 	public class Razor : Entity
 	{
 		public Image myImage;
@@ -21,7 +43,8 @@ namespace SNHU.GameObject
 		private float speed;
 		
 		public Image razorArm;
-		public Image theRazor;
+		
+		private RazorBlade theRazor;
 		
 		
 		public Razor()
@@ -31,36 +54,47 @@ namespace SNHU.GameObject
 		public override void Load(System.Xml.XmlNode node)
 		{
 			base.Load(node);
-			myImage = Image.CreateRect(16, 16, FP.Color(0x00FF00));
-			razorArm = Image.CreateRect(uint.Parse(node.Attributes["distance"].Value) * 32, 8, FP.Color(0xFFFFFF));
-			theRazor = Image.CreateCircle(uint.Parse(node.Attributes["size"].Value), FP.Color(0xFF0000));
+			myImage = new Image(Library.GetTexture("assets/pivot.png"));
+			razorArm = new Image(Library.GetTexture("assets/razorArm.png"));
 			
-			theRazor.CenterOO();
+			razorArm.ScaleX = float.Parse(node.Attributes["distance"].Value) * 32 / razorArm.Width;
+			
 			myImage.CenterOO();
 			razorArm.OriginY = razorArm.Height /2;
 			
-			razorArm.X = myImage.X;
-			razorArm.Y = myImage.Y;
+			theRazor = new RazorBlade();
+			theRazor.X = X;
+			theRazor.Y = Y;
 			
-			theRazor.X += razorArm.Width;
+			theRazor.X += razorArm.ScaledWidth;
 
 			speed = uint.Parse(node.Attributes["speed"].Value);
 			rotation = FP.Rand(360);
 			
 			AddGraphic(razorArm);
-			AddGraphic(theRazor);
 			AddGraphic(myImage);
 			SetHitbox((int)(myImage.Width), (int)(myImage.Height), (int)(myImage.X + myImage.Width/2), (int)(myImage.Y + myImage.Height/2));	
+		}
+		
+		public override void Added()
+		{
+			base.Added();
+			World.Add(theRazor);
+		}
+		
+		public override void Removed()
+		{
+			base.Removed();
+			World.Remove(theRazor);
 		}
 		
 		public override void Update()
 		{
 			base.Update();
 			rotation += speed;
-			FP.RotateAround(ref theRazor.X, ref theRazor.Y, myImage.X, myImage.Y, rotation, false);
+			FP.AnchorTo(ref theRazor.X, ref theRazor.Y, X, Y, razorArm.ScaledWidth);
+			FP.RotateAround(ref theRazor.X, ref theRazor.Y, X, Y, rotation, false);
 			razorArm.Angle = rotation;
-			
-			
 		}
 	}
 }
