2235cd4b6685e617a116724082ab08a78b251f82
Jacob Albano <Jacob Albano@localhost>
2013-11-23 18:52:43 -0500

    Code 1;

---
+++ SNHU/MenuWorld.cs
@@ -0,0 +1,203 @@
+﻿
+using System;
+using System.Collections.Generic;
+using SNHU.menuobject;
+using Punk;
+using Punk.Graphics;
+using Punk.Tweens.Misc;
+using Punk.Utils;
+using SFML.Window;
+
+namespace SNHU
+{
+	/// <summary>
+	/// Description of MenuWorld.
+	/// </summary>
+	public class MenuWorld : World
+	{
+		private Dictionary<uint, ControllerSelect> controllerMenus;
+		private Stack<int> pool;
+		
+		private List<string> allImages, takenImages;
+		
+		private bool readying;
+		
+		public MenuWorld()
+		{
+			allImages = new List<string>()
+			{
+				"batman.png",
+				"dragonborn.png",
+				"faith.png",
+				"gomez.png",
+				"gordon.png",
+				"punk.png",
+			};
+			
+			takenImages = new List<string>();
+			
+			controllerMenus = new Dictionary<uint, ControllerSelect>();
+			pool = new Stack<int>();
+			
+			for (uint i = 0; i < Joystick.Count; i++)
+			{
+				if (Joystick.IsConnected(i))
+				{
+					AddController(i);
+				}
+			}
+			
+			AddTween(new Alarm(0.1f, () => BroadcastMessage(ControllerSelect.ControllerAdded, GetSlot()), ONESHOT), true);
+		}
+		
+		public override void Begin()
+		{
+			base.Begin();
+			Input.ControllerConnected += OnControllerAdded;
+			Input.ControllerDisconnected += OnControllerRemoved;
+		}
+		
+		public override void End()
+		{
+			base.End();
+			Input.ControllerConnected -= OnControllerAdded;
+			Input.ControllerDisconnected -= OnControllerRemoved;
+		}
+		
+		public override void Update()
+		{
+			base.Update();
+			
+			if (readying)	return;
+			
+			if (Input.Pressed(Keyboard.Key.F5))
+			{
+				FP.World = new MenuWorld();
+			}
+			
+			foreach (var menu in controllerMenus.Values)
+			{
+				if (!menu.Ready)
+				{
+					return;
+				}
+			}
+			
+			if (controllerMenus.Count > 0)
+			{
+				AddTween(new Alarm(0.5f, Ready, ONESHOT), true);
+			}
+		}
+		
+		void Ready()
+		{
+			readying = true;
+			
+			var i = Image.CreateRect(FP.Width, FP.Height, FP.Color(0));
+			i.Alpha = 0;
+			
+			AddGraphic(i, -100);
+			
+			var tween = new VarTween(StartTheGame, ONESHOT);
+			tween.Tween(i, "Alpha", 1, 0.25f, Ease.SineOut);
+			AddTween(tween, true);
+		}
+		
+		void StartTheGame()
+		{
+			FP.World = new GameWorld();
+		}
+		
+		private void OnControllerAdded(object sender, JoystickConnectEventArgs e)
+		{
+			if (controllerMenus.ContainsKey(e.JoystickId))	return;
+			
+			AddController(e.JoystickId);
+		}
+		
+		void AddController(uint joystickId)
+		{
+			var slot = GetSlot();
+			if (slot > 3)
+			{
+				return;
+			}
+			
+			var menu = new ControllerSelect(this, slot, joystickId);
+			
+			menu.X = -menu.Width;
+			
+			controllerMenus[joystickId] = menu;
+			Add(menu);
+			
+			var tween = new VarTween(null, ONESHOT);
+			tween.Tween(menu, "X", (menu.Width / 2) + slot * menu.Width, 1.6f + FP.Random, Ease.ElasticOut);
+			AddTween(tween, true);
+		}
+		
+		private int GetSlot()
+		{
+			if (pool.Count > 0)
+			{
+				return pool.Pop();
+			}
+			
+			return controllerMenus.Count;
+		}
+		
+		private void OnControllerRemoved(object sender, JoystickConnectEventArgs e)
+		{
+			if (!controllerMenus.ContainsKey(e.JoystickId))	return;
+			
+			var menu = controllerMenus[e.JoystickId];
+			controllerMenus.Remove(e.JoystickId);
+			
+			pool.Push(menu.Slot);
+			
+			var tween = new VarTween(() => Remove(menu), ONESHOT);
+			tween.Tween(menu, "Y", -menu.Height, 0.75f + FP.Random / 2, Ease.ElasticOut);
+			AddTween(tween, true);
+			
+			BroadcastMessage(ControllerSelect.ControllerRemoved, menu.Slot);
+		}
+		
+		public string GetImageName()
+		{
+			var choice = "";
+			do {
+				choice = FP.Choose(allImages.ToArray());
+			} while (takenImages.IndexOf(choice) >= 0);
+			
+			takenImages.Add(choice);
+			return choice;
+		}
+		
+		public string NextImage(string current)
+		{
+			var c = current;
+			var choice = "";
+			do {
+				choice = FP.Next(c, allImages, true);
+				c = choice;
+			} while (takenImages.IndexOf(choice) >= 0);
+			
+			takenImages.Remove(current);
+			takenImages.Add(choice);
+			return choice;
+		}
+		
+		public string PrevImage(string current)
+		{
+			var c = current;
+			var choice = "";
+			do {
+				choice = FP.Prev(c, allImages, true);
+				c = choice;
+			} while (takenImages.IndexOf(choice) >= 0);
+			
+			takenImages.Remove(current);
+			takenImages.Add(choice);
+			return choice;
+		}
+	}
+}
---
+++ SNHU/menuobject/ControllerSelect.cs
@@ -0,0 +1,254 @@
+﻿
+using System;
+using System.Collections.Generic;
+using Punk;
+using Punk.Graphics;
+using Punk.Tweens.Misc;
+using Punk.Utils;
+using SFML.Window;
+using SNHU;
+
+namespace SNHU.menuobject
+{
+	/// <summary>
+	/// Description of ControllerSelect.
+	/// </summary>
+	public class ControllerSelect : Entity
+	{
+		public const string ControllerAdded = "controllerselect_added";
+		public const string ControllerRemoved = "controllerselect_removed";
+		
+		public int Slot { get; private set; }
+		public uint JoyId { get; private set; }
+		
+		public Controller Controller { get; private set; }
+		
+		public bool Ready {get; private set; }
+		
+		private Image image;
+		private Text text;
+		private Image player;
+		private Image lArrow, rArrow;
+		private Image confirm;
+		private string playerImageName;
+		private bool changing;
+		
+		private bool started;
+		
+		private MenuWorld parent;
+		
+		private static int[] colors;
+		static ControllerSelect()
+		{
+			colors = new int[]
+			{
+				0xFF1919,
+				0x4CFF4C,
+				0x4C4CFF,
+				0xFFFF4C
+			};
+		}
+		
+		public ControllerSelect(MenuWorld parent, int slot, uint joyId)
+		{
+			changing = false;
+			
+			this.parent = parent;
+			Height = FP.Height;
+			Width = 200;
+			
+			this.JoyId = joyId;
+			this.Slot = slot;
+			
+			image = new Image(Library.GetTexture("assets/menu.png"));
+			image.Color = FP.Color(colors[slot]);
+			text = new Text("PRESS START");
+			text.X -= 70;
+			text.Y = Height - 100;
+			text.Size = 20;
+			text.Color = FP.Color(0);
+			
+			OriginX = Width / 2;
+			image.OriginX = image.Width / 2;
+			
+			Controller = MakeController(JoyId);
+			
+			Controller.Define("Start", (int) JoyId, (Controller.Button) 9, Controller.Button.Start);
+			
+			AddGraphic(image);
+			AddGraphic(text);
+		}
+		
+		public override void Update()
+		{
+			base.Update();
+			
+			if (!started)
+			{
+				if (Controller.Pressed("Start"))
+				{
+					started = true;
+					
+					var list = Graphic as Graphiclist;
+					list.Remove(text);
+					
+					if (Joystick.HasAxis(JoyId, Joystick.Axis.PovX) && Joystick.HasAxis(JoyId, Joystick.Axis.PovY))
+					{
+						confirm = new Image(Library.GetTexture("assets/Xbox_1.png"));
+						Controller.Define("A", (int) JoyId, Controller.Button.A);
+					}
+					else
+					{
+						confirm = new Image(Library.GetTexture("assets/Snes_1.png"));
+						Controller.Define("A", (int) JoyId, Controller.Button.X);
+					}
+					
+					confirm.CenterOO();
+					confirm.Y = Height * 0.75f;
+					
+					AddGraphic(confirm);
+					
+					Y -= 50;
+					ClearTweens();
+					var tween = new VarTween(null, ONESHOT);
+					tween.Tween(this, "Y", 1, 0.75f, Ease.BounceOut);
+					AddTween(tween, true);
+					
+					playerImageName = parent.GetImageName();
+					MakePlayerImage();
+					
+					StartCursors();
+				}
+			}
+			else
+			{
+				if (!changing)
+				{
+					if (Controller.LeftStick.X < 0)
+					{
+						lArrow.Scale = 0.8f;
+						
+						var tween = new VarTween(null, ONESHOT);
+						tween.Tween(lArrow, "Scale", 1, 0.3f);
+						AddTween(tween, true);
+						
+						playerImageName = parent.NextImage(playerImageName);
+						MakePlayerImage();
+					}
+					else if (Controller.LeftStick.X > 0)
+					{
+						rArrow.Scale = 0.8f;
+						
+						var tween = new VarTween(null, ONESHOT);
+						tween.Tween(rArrow, "Scale", 1, 0.3f);
+						AddTween(tween, true);
+						
+						playerImageName = parent.PrevImage(playerImageName);
+						MakePlayerImage();
+					}
+					
+					if (Controller.Pressed("A"))
+					{
+						confirm.Scale = 0.8f;
+						confirm.Y += 20;
+					}
+					else if (Controller.Released("A"))
+					{
+						confirm.Scale = 1f;
+						confirm.Y -= 20;
+						
+						var tween = new VarTween(MakeCheckmark, ONESHOT);
+						tween.Tween(confirm, "ScaleY", 0, 0.1f, Ease.SineOut);
+						AddTween(tween, true);
+					}
+				}
+			}
+		}
+		
+		void MakeCheckmark()
+		{
+			var list = Graphic as Graphiclist;
+			list.Remove(lArrow);
+			list.Remove(rArrow);
+			
+			changing = true;
+			
+			var check = new Image(Library.GetTexture("assets/ready.png"));
+			check.CenterOO();
+			check.Y = Height * 0.75f;
+			check.ScaleY = 0;
+			list.Add(check);
+			
+			var tween = new VarTween(null, ONESHOT);
+			tween.Tween(check, "ScaleY", 1, 0.1f, Ease.SineOut);
+			AddTween(tween, true);
+			
+			Ready = true;
+		}
+		
+		void StartCursors()
+		{
+			var texture = Library.GetTexture("assets/change.png");
+			lArrow = new Image(texture);
+			rArrow = new Image(texture);
+			
+			lArrow.CenterOO();
+			rArrow.CenterOO();
+			
+			lArrow.FlippedX = true;
+			
+			lArrow.X = -60;
+			rArrow.X = 60;
+			
+			lArrow.Y = Height / 2;
+			rArrow.Y = Height / 2;
+			
+			AddGraphic(lArrow);
+			AddGraphic(rArrow);
+		}
+		
+		void MakePlayerImage()
+		{
+			changing = true;
+			
+			if (player != null)
+			{
+				var tween = new VarTween(ScalePlayerIn, ONESHOT);
+				tween.Tween(player, "Scale", 0, 0.1f);
+				AddTween(tween, true);
+			}
+			else
+			{
+				ScalePlayerIn();
+			}
+		}
+		
+		void ScalePlayerIn()
+		{
+			if (player != null)
+			{
+				var list = Graphic as Graphiclist;
+				list.Remove(player);
+			}
+			
+			player = new Image(Library.GetTexture("assets/" + playerImageName));
+			player.CenterOrigin();
+			
+			player.X = 0;
+			player.Y = Height / 2;
+			
+			player.Scale = 0;
+			
+			var tween = new VarTween(() => changing = false, ONESHOT);
+			tween.Tween(player, "Scale", 1, 0.35f);
+			AddTween(tween, true);
+			
+			AddGraphic(player);
+		}
+		
+		Controller MakeController(uint joyId)
+		{
+			return new Controller(joyId);
+		}
+	}
+}
